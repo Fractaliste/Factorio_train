0c2a5ce637d6db387527c3c1ace5f6e3fe73c9dd
unknown <fractaliste@gmail.com>
2018-05-16 13:25:58 +0200

    Ajout de la détection d'un OuOfFuel

--- controle.lua
+++
@@ -1,8 +0,0 @@
-
-function updateSchedule(train, state)
-  game.write_file("mod.log", "Event - Hello world!", true)
-end
-
-game.write_file("mod.log", "Hello world!", true)
-
-script.on_event(defines.events.on_train_changed_state, updateSchedule)
