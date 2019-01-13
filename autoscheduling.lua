a77aff12a6eb50fe1f5533f6c9fc939f9cdac3c1
Fractaliste <fractaliste@gmail.com>
2019-01-13 16:08:23 +0100

    WIP : suite d'implémentation de l'autoscheduling (onRename)

--- autoscheduling.lua
+++ autoscheduling.lua
@@ -1,52 +1,82 @@
 local stations = {}
 local trains = {}
 
-local isManagedStation
+local isElligibleStation
 local isElligibleName
+local createName
 
 local managedStationColor = {g = 1, a = 0.5}
 local unManagedStationColor = {b = 1, a = 0.5}
 Autoscheduling = {
-    onRename = function(event) -- Est-ce que la station appartient ou appartenait au réseau ?
-        debug(event.entity.get_train_stop_trains())
-        if isManagedStation(event.entity) then
-            event.entity.color = managedStationColor
+    onRename = function(event)
+        if (event.name == defines.events.on_entity_renamed) then
+            destEntity = event.entity
+            oldName = event.old_name
+        else
         end
-        -- Synchroniser la liste des stations qu'on gère
 
-        -- Vérifier l'ancien nom s'il était managé
+        -- Est-ce que la station appartient au réseau ?
+        if isElligibleStation(event.entity) then
+            -- Si la station est déjà managée c'est qu'on vient sûrement de la renommer, il faut donc couper cours pour éviter une récursion infinie de renommage
+            if isSamePosition(stations[event.entity.backer_name], event.entity) then
+                return
+            end
+
+            newName = createName(event.entity.backer_name)
+            stations[newName] = event.entity
+            event.entity.backer_name = newName
+
+            event.entity.color = managedStationColor
+        else
+            event.entity.color = unManagedStationColor
+        end
+
+        -- Est-ce que la station appartenait au réseau ?
+        if isElligibleName(event.old_name) then
+            stations[event.old_name] = nil
+        end
     end,
     onBuilt = function(event)
-        -- Si le nom existe déjà, en recréer un
-        debug(isManagedStation(event.created_entity))
+        -- Si le nom existe déjà il faudrait en recréer un, mais c'est pas avant la 0.17 normallement
+        if isElligibleStation(destEntity) then
+            destEntity.color = managedStationColor
+        else
+            event.created_entity.color = unManagedStationColor
+        end
     end,
     onRemoved = function(event)
-        debug(event.entity.backer_name)
+        debug(event)
     end,
     onTrainChangedState = function(event)
-        -- Un train faisant parti de l'autoscheduling est un train en mode automatique et qui a comme dernière station prévue le parking
-        if
-            event.train.manual_mode or event.train.schedule == nil or
-                event.train.schedule.records[#event.train.schedule.records] ~= cache["autoschedule_parking_name"]
-         then
-            trains[event.train.id] = nil --Reset si jamais ce train avait précédemment fait partie de l'autoscheduling
+        -- Les trains en mode manuel ne sont pas éligible
+        if event.train.manual_mode then
             return
         end
 
-        -- Si le train va vers le parking, vérifier pour lui attribuer un nouveau boulot
+        -- Seuls les trains en route pour le parking sont réassignable
+        if event.train.schedule.records[event.train.schedule.current] == cache["autoschedule_parking_name"] then
+        -- Vérifier s'il faut recommencer leur mission
 
-        -- Sinon le laisser faire son boulot
+        -- Sinon vérifier s'il faut leur attribuer une nouvelle mision
+        end
+
+        -- Si on est arrivé au parking
+        if true then
+        -- Vider le planning
+        -- Ajouter à la liste des idlsTrains
+        end
     end,
     onStationStateUpdate = function(event)
         -- Pour chaque station gérée faire
         -- -- Si aucun train sur la route (entity.get_train_stop_trains()) se mettre sur la liste d'attente
     end,
     onTraiterListeAttente = function()
+        -- Pour chaque entrée
     end
 }
 
 -- Retourne true si l'entité est une station, et est dans le scope de l'autoscheduling
-isManagedStation = function(entity)
+isElligibleStation = function(entity)
     if (entity.name == "train-stop") then
         return isElligibleName(entity.backer_name)
     else
@@ -56,4 +86,22 @@ end
 
 isElligibleName = function(name)
     return string.find(name, ".+#[io].+") ~= nil
+end
+
+isSamePosition = function(entity_1, entity_2)
+    if entity_1 == nil or entity_2 == nil then
+        return false
+    else
+        return entity_1.position.x == entity_2.position.x and entity_1.position.y == entity_2.position.y
+    end
+end
+
+createName = function(baseName)
+    -- S'il y a déjà deux dièses
+    if string.find(baseName, "#.*#") then
+        return string.gsub(baseName, "(#.-#).*", "%1" .. math.random(100000))
+    else
+        -- Sinon on est dans le cas classique où on se contente d'une concaténation pour rajouter le second fameux dièse
+        return baseName .. "#" .. math.random(100000)
+    end
 end
--- event.lua
+++ event.lua
@@ -9,8 +9,11 @@ script.on_event(defines.events.on_runtime_mod_setting_changed, init_cache)
 
 if cache["autoschedule_enabled"] then
     script.on_event(defines.events.on_entity_renamed, Autoscheduling.onRename)
+    script.on_event(defines.events.on_entity_settings_pasted, Autoscheduling.onRename)
     script.on_event(defines.events.on_built_entity, Autoscheduling.onBuilt)
     script.on_event(defines.events.on_robot_built_entity, Autoscheduling.onBuilt)
     script.on_event(defines.events.on_player_mined_entity, Autoscheduling.onRemoved)
+    script.on_event(defines.events.on_robot_mined_entity, Autoscheduling.onRemoved)
+    script.on_event(defines.events.on_entity_died, Autoscheduling.onRemoved)
     script.on_event(defines.events.on_train_changed_state, Autoscheduling.onTrainChangedState)
 end
